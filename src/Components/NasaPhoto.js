9854c1770863a5a36df34959af998d3931bb5bea
Adam Smith <91632531+Roadlyfe@users.noreply.github.com>
2022-03-24 15:59:12 -0500

    page stylings

--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -1,13 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledPage = styled.div`
+  p {
+    color: ${(pr) => pr.theme.color};
+    font-size: 2rem;
+  }
+  h1 {
+    font-size: 5rem;
+    color: ${(pr) => pr.theme.secondaryColor};
+  }
+  .explanation {
+    max-width: 65%;
+    font-size: 1.6rem;
+    color: ${(pr) => pr.theme.tertiaryColor};
+  }
+`;
 
 const NasaPhoto = (props) => {
   return (
-    <div className="nasa-photo-wrapper">
+    <StyledPage className="nasa-photo-wrapper">
       <h1>{props.photo.title}</h1>
       <p>{props.photo.date}</p>
       <img src={props.photo.hdurl} />
       <p className="explanation">{props.photo.explanation}</p>
-    </div>
+    </StyledPage>
   );
 };
 export default NasaPhoto;
---
+++ src/theme/index.js
@@ -0,0 +1,9 @@
+export default {
+  primaryColor: "#0077B6",
+  secondaryColor: "#0096C7",
+  tertiaryColor: "#CAF0F8",
+  black: "black",
+  white: "white",
+  breakpointMobile: "(max-width: 550px)",
+  color: "#CAF0F8",
+};
